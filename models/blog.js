74dd2ff153b22ecd5f8f8f0560ea43d20bff6036
Steven G <stevenglab@gmail.com>
2021-02-14 17:40:03 -0700

    added required true on draft

--- models/blog.js
+++ models/blog.js
@@ -6,7 +6,7 @@ var Schema = mongoose.Schema;
 var BlogSchema = new Schema({
     title: {type: String, required: true, maxlength: 100},
     body: {type: String, required: true, maxlength: 250},
-    draft: {type: Boolean, default: true},
+    draft: {type: Boolean, default: true, required: true},
     datePosted: {type: Date, required: true},
     comments: [{type: Schema.Types.ObjectId, ref: 'Comment', required: true}],
     postedBy: { type: Schema.Types.ObjectId, ref: 'User', required: true }
